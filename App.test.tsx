45f5acf1aa5de3bfc9b1843eaea2c81be9437219
mirza-bilal-amjad <69304697+mirza-bilal-amjad@users.noreply.github.com>
2023-11-29 15:54:26 +0500

    Added add item and remove item functionality

--- App.test.tsx
+++ App.test.tsx
@@ -1,23 +1,28 @@
-import 'react-native';
 import React from 'react';
+import { render, waitFor } from '@testing-library/react-native';
 import App from './App';
-
-// Note: import explicitly to use the types shiped with jest.
-import {it} from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
-import {Provider} from 'react-redux';
-import store from './src/store/store';
-import {render} from '@testing-library/react-native';
-
-it('App renders correctly', () => {
-    const component = (
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    );
-
-    render(component);
-
-    expect(component).toBeDefined();
+import renderer from 'react-test-renderer';
+// Mock the ImagePicker module
+jest.mock('expo-image-picker', () => ({
+    requestMediaLibraryPermissionsAsync: jest.fn(() => ({ status: 'granted' })),
+    requestCameraPermissionsAsync: jest.fn(() => ({ status: 'granted' })),
+}));
+// Mock the StatusBar module
+jest.mock('expo-status-bar', () => ({
+    StatusBar: jest.fn(),
+}));
+describe('<App />', () => {
+    it('renders Correctly', ()=>{
+        const tree =   renderer.create(<App />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+    it('requests media library and camera permissions', async () => {
+        render(<App />);
+        // Wait for the asynchronous code to complete
+        await waitFor(() => {
+            expect(require('expo-image-picker').requestMediaLibraryPermissionsAsync).toHaveBeenCalled();
+            expect(require('expo-image-picker').requestCameraPermissionsAsync).toHaveBeenCalled();
+        });
+    });
+    // Add more test cases based on your application logic
 });
